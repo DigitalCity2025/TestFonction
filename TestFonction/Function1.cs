ece8a815778589497595bf984f3def54206431d1
Khun <lykhun@gmail.com>
2025-04-01 14:14:35 +0200

    Add project files.

---
+++ TestFonction/Function1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using TestFonction.Models;
+
+namespace TestFonction
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([TimerTrigger("0 30 12 * * 1 ")]TimerInfo myTimer, ILogger log)
+        {
+            using HttpClient client = new HttpClient();
+
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            IConfigurationRoot config = builder.Build();
+
+            HttpResponseMessage message = client.PostAsJsonAsync(
+                config["apiUrl"] + "/api/login", new { Username = config["username"], Password = config["password"] }).Result;
+            if(message.IsSuccessStatusCode)
+            {
+                string token = message.Content.ReadAsAsync<TokenDTO>().Result.Token;
+                log.LogInformation(token);
+            }
+            // TODO implement what you want
+        }
+    }
+}
